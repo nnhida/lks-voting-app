f163a08f2eda3b7c3080261fc15021f139439daf
nnhida <nurhidayahayundira296@gmail.com>
2025-01-29 10:18:03 +0700

    push dev

--- vote/app.py
+++ vote/app.py
@@ -18,7 +18,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(host="master.lks-redis.4z141i.use1.cache.amazonaws.com", port=6379, db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
