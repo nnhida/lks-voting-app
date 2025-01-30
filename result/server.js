11db4409bc03175b054013aefc18d01ad98fae21
nnhida <nurhidayahayundira296@gmail.com>
2025-01-30 16:18:38 +0700

    gdffgfg

--- result/server.js
+++ result/server.js
@@ -18,7 +18,7 @@ io.on('connection', function (socket) {
 });
 
 var pool = new Pool({
-  connectionString: 'postgres://lksadmin:LKSNCC2024@lks-rds.ckb2qs0we42t.us-east-1.rds.amazonaws.com/lksdb'
+  connectionString: 'postgres://lksadmin:LKSNCC2024@lks-rds.ckb2qs0we42t.us-east-1.rds.amazonaws.com/lksadmin'
 });
 
 async.retry(
