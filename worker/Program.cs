01f796dcfcebf6eddea7cfb109b53a6eb750ee99
nnhida <nurhidayahayundira296@gmail.com>
2025-01-30 14:30:55 +0700

    push

--- worker/Program.cs
+++ worker/Program.cs
@@ -16,7 +16,7 @@ namespace Worker
         {
             try
             {
-                var pgsql = OpenDbConnection("Server=lks-rds.ckb2qs0we42t.us-east-1.rds.amazonaws.com;Username=lksadmin;Password=LKSNCC2024");
+                var pgsql = OpenDbConnection("Server=lks-rds.ckb2qs0we42t.us-east-1.rds.amazonaws.com;Database=lksdb;Username=lksadmin;Password=LKSNCC2024;");
                 var redisConn = OpenRedisConnection("wokwokwok-001.4z141i.0001.use1.cache.amazonaws.com");
                 var redis = redisConn.GetDatabase();
 
@@ -46,7 +46,7 @@ namespace Worker
                         if (!pgsql.State.Equals(System.Data.ConnectionState.Open))
                         {
                             Console.WriteLine("Reconnecting DB");
-                            pgsql = OpenDbConnection("Server=lks-rds.ckb2qs0we42t.us-east-1.rds.amazonaws.com;Username=lksadmin;Password=LKSNCC2024");
+                            pgsql = OpenDbConnection("Server=lks-rds.ckb2qs0we42t.us-east-1.rds.amazonaws.com;Database=lksdb;Username=lksadmin;Password=LKSNCC2024;");
                         }
                         else
                         { // Normal +1 vote requested
