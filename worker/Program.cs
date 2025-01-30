65c95bb9d4041670acbc2d3772ed32dc2873071d
nnhida <nurhidayahayundira296@gmail.com>
2025-01-30 09:42:43 +0700

    push prod

--- worker/Program.cs
+++ worker/Program.cs
@@ -16,8 +16,8 @@ namespace Worker
         {
             try
             {
-                var pgsql = OpenDbConnection("Server=lks-rds.ckb2qs0we42t.us-east-1.rds.amazonaws.com;Username=lksadmin;Password=LKSNCC2024;");
-                var redisConn = OpenRedisConnection("master.lks-redis.4z141i.use1.cache.amazonaws.com");
+                var pgsql = OpenDbConnection("Server=lks-rds.ckb2qs0we42t.us-east-1.rds.amazonaws.com;Username=lksadmin;Password=LKSNCC2024");
+                var redisConn = OpenRedisConnection("wokwokwok-ro.4z141i.ng.0001.use1.cache.amazonaws.com:6379");
                 var redis = redisConn.GetDatabase();
 
                 // Keep alive is not implemented in Npgsql yet. This workaround was recommended:
@@ -34,7 +34,7 @@ namespace Worker
                     // Reconnect redis if down
                     if (redisConn == null || !redisConn.IsConnected) {
                         Console.WriteLine("Reconnecting Redis");
-                        redisConn = OpenRedisConnection("master.lks-redis.4z141i.use1.cache.amazonaws.com");
+                        redisConn = OpenRedisConnection("wokwokwok-ro.4z141i.ng.0001.use1.cache.amazonaws.com:6379");
                         redis = redisConn.GetDatabase();
                     }
                     string json = redis.ListLeftPopAsync("votes").Result;
@@ -46,7 +46,7 @@ namespace Worker
                         if (!pgsql.State.Equals(System.Data.ConnectionState.Open))
                         {
                             Console.WriteLine("Reconnecting DB");
-                            pgsql = OpenDbConnection("Server=lks-rds.ckb2qs0we42t.us-east-1.rds.amazonaws.com;Username=lksadmin;Password=LKSNCC2024;");
+                            pgsql = OpenDbConnection("Server=lks-rds.ckb2qs0we42t.us-east-1.rds.amazonaws.com;Username=lksadmin;Password=LKSNCC2024");
                         }
                         else
                         { // Normal +1 vote requested
