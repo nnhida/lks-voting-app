7b50e221e8cd3cf336193a1453bcc1ad1167cebf
EC2 Default User <ec2-user@ip-172-31-87-204.ec2.internal>
2025-02-23 02:01:19 +0000

    push dev

--- result/server.js
+++ result/server.js
@@ -65,7 +65,7 @@ function collectVotesFromResult(result) {
 
 app.use(cookieParser());
 app.use(express.urlencoded());
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname + '/views/index.html'));
